fd2feab8346269927f50c07535b08565b49232cd
Tim Jones <jonestimd@gmail.com>
2019-12-05 21:48:51 -0600

    Display list of accounts

--- web/src/lib/component/AccountsPage.tsx
+++ web/src/lib/component/AccountsPage.tsx
@@ -9,13 +9,24 @@ const AccountsPage: React.FC<{}> = observer(() => {
         <div className='account-list'>
             <table className='table'>
                 <thead>
-                    <tr><th>Company</th><th>Name</th></tr>
+                    <tr>
+                        <th>Closed</th>
+                        <th>Company</th>
+                        <th>Name</th>
+                        <th>Type</th>
+                        <th>Description</th>
+                        <th>Account Number</th>
+                    </tr>
                 </thead>
                 <tbody>
                     {accounts.map(account => (
                         <tr>
+                            <td className='check-mark'>{account.closed ? <span>&#x1F5F8;</span> : null}</td>
                             <td>{account.companyName}</td>
                             <td>{account.name}</td>
+                            <td>{account.type}</td>
+                            <td>{account.description || ''}</td>
+                            <td>{account.accountNo || ''}</td>
                         </tr>
                     ))}
                 </tbody>

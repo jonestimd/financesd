5b9d9413631cff7a2a7dc048a315d15130b9a076
Tim Jones <jonestimd@gmail.com>
2020-12-20 10:19:34 -0600

    Add header row

--- web/src/lib/component/transaction/TransactionsPage.tsx
+++ web/src/lib/component/transaction/TransactionsPage.tsx
@@ -9,6 +9,8 @@ import TxDetail from './TxDetail';
 import Payee from './Payee';
 import Security from './Security';
 import Memo from './Memo';
+import TransactionModel from 'src/lib/model/TransactionModel';
+import {translate} from '../../i18n/localize';
 
 interface IProps {
     match: {params: {[name: string]: string}};
@@ -17,6 +19,38 @@ interface IProps {
 function numberClass(value: number, classes?: string) {
     return classNames(classes, 'number', {negative: value < 0});
 }
+
+const Header: React.FC = () => (
+    <div className='transaction header'>
+        <div className='leading'><span className='date'>{translate('transaction.date')}</span></div>
+        <div className='details'>{translate('transaction.details')}</div>
+        <div className='trailing'>
+            <i className='material-icons md-18'>check_box</i>
+            <span className='number'>{translate('transaction.subtotal')}</span>
+            <span className='number'>{translate('transaction.balance')}</span>
+        </div>
+    </div>
+);
+
+const Transaction: React.FC<{tx: TransactionModel}> = observer(({tx}) => (
+    <div className='transaction'>
+        <div className='leading'>
+            <span className='date'>{tx.date}</span>
+            {tx.referenceNumber ? <span className='ref-number'>{tx.referenceNumber}</span> : null}
+        </div>
+        <div className='details'>
+            <Payee transaction={tx} />
+            <Security transaction={tx} />
+            <Memo text={tx.memo} />
+            {tx.details.map(detail => <TxDetail key={detail.id} detail={detail} />)}
+        </div>
+        <div className='trailing'>
+            <input type='checkbox' checked={tx.cleared} readOnly />
+            <span className={numberClass(tx && tx.subtotal)}>{formats.currency.format(tx.subtotal)}</span>
+            <span className={numberClass(tx && tx.balance)}>{formats.currency.format(tx.balance)}</span>
+        </div>
+    </div>
+));
 
 const TransactionsPage: React.FC<IProps> = observer(({match: {params: {accountId}}}) => {
     const {accountStore, categoryStore, payeeStore, securityStore, transactionStore} = React.useContext(RootStoreContext);
@@ -32,25 +66,8 @@ const TransactionsPage: React.FC<IProps> = observer(({match: {params: {accountId
         <>
             <TopAppBar title={account ? account.displayName : ''} menuItems={<PageMenu />} />
             <div className='scroll-container'>
-                {transactionStore.getTransactionsModel(accountId).transactions.map(tx => (
-                    <div key={tx.id} className='transaction'>
-                        <div className='leading'>
-                            <span className='date'>{tx.date}</span>
-                            {tx.referenceNumber ? <span className='ref-number'>{tx.referenceNumber}</span> : null}
-                        </div>
-                        <div className='details'>
-                            <Payee transaction={tx} />
-                            <Security transaction={tx} />
-                            <Memo text={tx.memo} />
-                            {tx.details.map(detail => <TxDetail key={detail.id} detail={detail} />)}
-                        </div>
-                        <div className='trailing'>
-                            <input type='checkbox' checked={tx.cleared} readOnly />
-                            <span className={numberClass(tx && tx.subtotal)}>{formats.currency.format(tx.subtotal)}</span>
-                            <span className={numberClass(tx && tx.balance)}>{formats.currency.format(tx.balance)}</span>
-                        </div>
-                    </div>
-                ))}
+                <Header />
+                {transactionStore.getTransactionsModel(accountId).transactions.map(tx => <Transaction key={tx.id} tx={tx} />)}
             </div>
         </>
     );
